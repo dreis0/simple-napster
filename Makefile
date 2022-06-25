e5fb0e8d9a2685afa66a613aa4f606a561a85496
Miguel dos Reis <migueldreis01@gmai.com>
2022-06-25 18:22:41 -0300

    Makefile command to start server

--- Makefile
+++ Makefile
@@ -1,4 +1,8 @@
+PORT?=8000
 
 generate-protos:
 	 @protoc protos/*.proto --go_out=protos/messages --go-grpc_out=protos/services
 	 @echo Protobuf files generated
+
+run-server:
+	@go run server/*.go ${PORT}
--- server/main.go
+++ server/main.go
@@ -1,15 +1,26 @@
 package main
 
 import (
+	"fmt"
 	"google.golang.org/grpc"
 	"google.golang.org/grpc/reflection"
 	"log"
 	"net"
+	"os"
 	services "simple-napster/protos/services"
 )
 
 func main() {
-	listener, err := net.Listen("tcp", ":8080")
+
+	args := os.Args
+	if len(args) <= 1 {
+		panic("no port provided")
+	}
+
+	port := args[1]
+	log.Printf("port: %s", port)
+	
+	listener, err := net.Listen("tcp", fmt.Sprintf(":%s", port))
 	if err != nil {
 		panic(err)
 	}
@@ -21,4 +32,6 @@ func main() {
 	if err := s.Serve(listener); err != nil {
 		log.Fatalf("failed to serve: %v", err)
 	}
+
+	log.Printf("listening on port %s", args[1])
 }
