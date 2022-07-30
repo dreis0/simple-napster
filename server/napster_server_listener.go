2af4ca617d4ab8b168e9ee5133d36f444f567594
Miguel dos Reis <migueldreis01@gmai.com>
2022-07-30 15:22:13 -0300

    Improve logs on server side

--- server/napster_server_listener.go
+++ server/napster_server_listener.go
@@ -56,6 +56,8 @@ func (sl *NapsterServerListener) Start() {
 
 // Join TODO: check if bun correctly sets IDs and see how is the data in the database after a JOIN request
 func (sl *NapsterServerListener) Join(ctx context.Context, args *messages.JoinArgs) (*messages.JoinResponse, error) {
+	fmt.Printf("Join request received \n peer_ip: %s \n peer_port: %d \n", args.IP, args.Port)
+
 	peer := &entities.Peer{
 		Port:   args.Port,
 		Active: true,
@@ -63,6 +65,7 @@ func (sl *NapsterServerListener) Join(ctx context.Context, args *messages.JoinAr
 	}
 	err := sl.dal.AddPeer(ctx, peer)
 	if err != nil {
+		fmt.Printf("Error when trying to persist peer: %s", err.Error())
 		return nil, err
 	}
 
@@ -73,33 +76,40 @@ func (sl *NapsterServerListener) Join(ctx context.Context, args *messages.JoinAr
 
 	err = sl.dal.AddFilesIfNew(ctx, files)
 	if err != nil {
+		fmt.Printf("Error when adding new peer files: %s", err.Error())
 		return nil, err
 	}
 
 	err = sl.dal.AddFilesToPeer(ctx, peer, files)
 	if err != nil {
+		fmt.Printf("Error when associating files with peer: %s", err.Error())
 		return nil, err
 	}
 	return &messages.JoinResponse{Id: peer.ID.String()}, nil
 }
 
 func (sl *NapsterServerListener) Leave(ctx context.Context, args *messages.LeaveArgs) (*emptypb.Empty, error) {
+	fmt.Printf("Leave request received peer_id: %s\n", args.PeerId)
 	id, err := uuid.Parse(args.PeerId)
 	if err != nil {
+		fmt.Println("Invalid peer id")
 		return nil, err
 	}
 
 	err = sl.dal.DeletePeerAndFiles(ctx, id)
 	if err != nil {
-		return nil, err
+		fmt.Printf("Error when deleting peer files: %s \n", err.Error())
 	}
 
 	return &emptypb.Empty{}, nil
 }
 
 func (sl *NapsterServerListener) Search(ctx context.Context, args *messages.SearchArgs) (*messages.SearchResponse, error) {
+	fmt.Printf("Search request received for file: %s \n", args.Filename)
+
 	peers, err := sl.dal.GetAllPeersWithFile(ctx, args.Filename)
 	if err != nil {
+
 		return nil, err
 	}
 
@@ -115,6 +125,7 @@ func (sl *NapsterServerListener) Search(ctx context.Context, args *messages.Sear
 }
 
 func (sl *NapsterServerListener) Update(ctx context.Context, args *messages.UpdateArgs) (*emptypb.Empty, error) {
+	fmt.Printf("Update request received for peer %s \n", args.PeerId)
 	id, err := uuid.Parse(args.PeerId)
 	if err != nil {
 		return nil, err
@@ -123,6 +134,7 @@ func (sl *NapsterServerListener) Update(ctx context.Context, args *messages.Upda
 
 	err = sl.dal.AddFileToPeeerWithFilename(ctx, peer, args.NewFile)
 	if err != nil {
+		fmt.Printf("Error when persisting files for peer: %s \n", err.Error())
 		return nil, err
 	}
 
